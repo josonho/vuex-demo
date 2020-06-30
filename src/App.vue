c940fd30a10e72577ac33e3a40f54d337302bdf1
hezhaoshun <373693643@qq.com>
2020-06-30 20:02:46 +0800

    mapGetters

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <div>full name: {{ fullName }}</div>
     <div 
       v-for="item in doneTodos" 
-      :key="item"
+      :key="item.id"
     >
       done todo: {{ item.text }}
     </div>
@@ -16,7 +16,7 @@
 
 <script>
 import home from './components/home.vue'
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 
 export default {
   name: 'App',
@@ -36,20 +36,13 @@ export default {
       fullName() {
         return `${this.userName} ${this.firstName}`;
       },
-
-      doneTodos () {
-        return this.$store.getters.doneTodos
-      },
-      doneTodosCount () {
-        return this.$store.getters.doneTodosCount
-      }
     }),
 
     // 当映射的计算属性的名称与 state 的子节点名称相同时，我们也可以给 mapState 传一个字符串数组
-    // ...mapState([
-    //   'count',
-    //   'userName'
-    // ])
+    ...mapGetters([
+      'doneTodos',
+      'doneTodosCount'
+    ])
   },
 }
 </script>
